f18a0c197887ab17717c9b453954afff4c68c36a
Cecilia Ou <cou.code@gmail.com>
2023-05-03 00:17:48 -0700

    Updated JavaScript solution to longest substring

--- neetcode/blind75/12-longest-substring-without-repeating-characters.js
+++ neetcode/blind75/12-longest-substring-without-repeating-characters.js
@@ -24,24 +24,22 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 */
-let lengthOfLongestSubstring = function(s) {
-   let table = {}
-   let maxCount = 0;
-   if (s.length === 0) return 0
+let lengthOfLongestSubstring = function (s) {
+    let maxCount = 0;
+    if (s.length === 0) return 0
 
-   for (let i = 0; i < s.length; i++){
-       let count = 0;
-       let str = s[i]
-       for (let j = i + 1; j < s.length; j++){
-           if (str.indexOf(s[j]) === -1){
-               str = str + s[j]
-               count++
-               table[count] = str
-               maxCount = Math.max(count, maxCount)
-           } else {
-               break
-           }
-       }
-   }
-   return maxCount + 1
+    for (let i = 0; i < s.length; i++) {
+        let count = 0;
+        let str = s[i]
+        for (let j = i + 1; j < s.length; j++) {
+            if (str.indexOf(s[j]) === -1) {
+                str = str + s[j]
+                count++
+                maxCount = Math.max(count, maxCount)
+            } else {
+                break
+            }
+        }
+    }
+    return maxCount + 1
 };
