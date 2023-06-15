0b2cf6a4ddada0d62b4497f2338aadd6a82f0cf1
Cecilia Ou <cou.code@gmail.com>
2023-06-15 16:14:42 -0700

    Linked List Values Completed in JavaScript and Python

---
+++ structy/2-linked-list/python/linked-list-values.py
@@ -0,0 +1,48 @@
+"""
+linked list values
+Write a function, linked_list_values, that takes in the head of a linked list as an argument. 
+The function should return a list containing all values of the nodes in the linked list.
+
+Hey. This is our first linked list problem, so you should be liberal with watching the Approach and Walkthrough. Be productive! -AZ
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+# a -> b -> c -> d
+
+linked_list_values(a) # -> [ 'a', 'b', 'c', 'd' ]
+x = Node("x")
+y = Node("y")
+
+x.next = y
+
+# x -> y
+
+linked_list_values(x) # -> [ 'x', 'y' ]
+q = Node("q")
+
+# q
+
+linked_list_values(q) # -> [ 'q' ]
+linked_list_values(None) # -> [ ]
+"""
+# Iterative Approach
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def linked_list_values(head):
+  values = []
+  
+  current = head
+  while current is not None:
+    values.append(current.val)
+    current = current.next
+  return values
