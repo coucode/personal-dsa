1dd8ceb8c3ba2dec185b7273861acc8ff4ae6b4f
Cecilia Ou <cou.code@gmail.com>
2023-06-29 09:12:37 -0700

    Largest Component Completed in JavaScript and Python

---
+++ structy/4-graph/python/4-largest-component.py
@@ -0,0 +1,68 @@
+"""
+largest component
+Write a function, largest_component, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph.
+
+largest_component({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}) # -> 4
+largest_component({
+  1: [2],
+  2: [1,8],
+  6: [7],
+  9: [8],
+  7: [6, 8],
+  8: [9, 7, 2]
+}) # -> 6
+largest_component({
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1]
+}) # -> 5
+largest_component({}) # -> 0
+largest_component({
+  0: [4,7],
+  1: [],
+  2: [],
+  3: [6],
+  4: [0],
+  6: [3],
+  7: [0],
+  8: []
+}) # -> 3
+"""
+
+def largest_component(graph):
+  largest = 0
+  visited = set()
+  
+  for node in graph:
+    if node not in visited:
+      visited.add(node)
+      stack = [node]
+      size = 0
+      
+      while stack:
+        current = stack.pop()
+        size += 1
+        
+        if size > largest:
+          largest = size
+          
+        for neighbor in graph[current]:
+          if neighbor not in visited:
+            stack.append(neighbor)
+            visited.add(neighbor)
+        
+  
+  return largest
